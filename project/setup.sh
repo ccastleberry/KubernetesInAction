b2df991c1b86cf8a339f8cf23dd019aeac58976b
Caleb Castleberry <castle.caleb@gmail.com>
2019-11-14 10:10:24 -0500

    added files to spin up and shut down kubernetes cluster automatically

---
+++ project/setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gcloud container clusters create kubia --num-nodes 3 --zone us-east1-b
---
+++ project/tear_down.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gcloud container clusters delete kubia --zone us-east1-b
